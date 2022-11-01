8734b6a2f7ee93d073017e02677a2216c6cb89a4
thabo <thabimbele1@gmail.com>
2022-11-01 14:49:20 +0200

    Changed the View

--- src/main/java/com/stage/one/model/ModelClass.java
+++ src/main/java/com/stage/one/model/ModelClass.java
@@ -12,17 +12,17 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class ModelClass {
 
-	private String slackUsername = "Thiza";
+	private String slackUsername = "\"Thiza\"";
 	private boolean backend = true;
 	private int age = 26;
-	private String bio = "A tech-head that speaks fluent java,"
-			+ "Im an easy-going guy with very strong work ethics.";
+	private String bio = "\"A tech-head that speaks fluent java,"
+			+ "Im an easy-going guy with very strong work ethics.\"";
 	
 	
 	
 	public String showData() {
-		return "{"+ "\"slackUsername:\""+slackUsername+", \"backend:\""+backend+",\"age:\""+age
-				+", \"bio:\""+bio+"}";
+		return "\"slackUsername\":"+slackUsername+", \"backend\":"+backend+",\"age\":"+age
+				+", \"bio\":"+bio;
 	}
 	
 	
